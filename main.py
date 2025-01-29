fbafbe76a103bcb59c39a8c3c39bd29a6c89f3f9
Grsneed <gino.sneed@gmail.com>
2025-01-28 19:17:54 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def in_autotest_we_trust (a, b):
+    if a == b:
+        print('Test passed')
+    else;
+        print('Test failed')
+in_autotest_we_trust(10, '10')
+in_autotest_we_trust((0, False)
